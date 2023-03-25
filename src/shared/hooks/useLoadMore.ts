79da610737a8407f41ce9009e9e646b378468d89
Pedro-Afonso <pedro.766@hotmail.com>
2023-03-25 20:44:16 -0300

    feat: Export 'canLoadMore' variable

--- src/shared/hooks/useLoadMore.ts
+++ src/shared/hooks/useLoadMore.ts
@@ -5,23 +5,33 @@ export const useLoadMore = <T>(
   limit: number,
   start: number
 ) => {
+  const [canLoadMore, setCanLoadMore] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
+  const [nextPage, setNextPage] = useState(0)
 
   const loadMore = async () => {
     setIsLoading(true)
 
     try {
       const res = await fetch(
-        `https://jsonplaceholder.typicode.com/${endpoint}?_start=${start}&_limit=${limit}`
+        `https://jsonplaceholder.typicode.com/${endpoint}?_start=${
+          start + nextPage * limit
+        }&_limit=${limit}`
       )
       const data = (await res.json()) as T[]
+      if (data?.length < limit) {
+        setCanLoadMore(false)
+      }
       return data
-      setIsLoading(false)
     } catch (error) {
+      setCanLoadMore(false)
       // eslint-disable-next-line no-console
       console.log(error)
+    } finally {
+      setNextPage(prev => prev + 1)
+      setIsLoading(false)
     }
   }
 
-  return { isLoading, loadMore }
+  return { loadMore, isLoading, canLoadMore }
 }
