715cd3e633ea10a8bdda5845746529fd8722dc36
Pedro-Afonso <pedro.766@hotmail.com>
2023-03-25 20:46:15 -0300

    feat: Adds the functionality to load more posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,19 +2,21 @@ import { InferGetStaticPropsType } from 'next'
 
 import { PostListing, TPost } from '@/shared/components'
 
+const limit = 9
+
 export default function Posts({
   posts
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <main>
-      <PostListing posts={posts} />
+      <PostListing posts={posts} limit={limit} />
     </main>
   )
 }
 
 export async function getStaticProps() {
   const res = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=10'
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
   )
   const posts = (await res.json()) as TPost[]
 
--- src/shared/components/PostListing/PostListing.tsx
+++ src/shared/components/PostListing/PostListing.tsx
@@ -1,3 +1,4 @@
+import { useLoadMore } from '@/shared/hooks'
 import { FC, useState } from 'react'
 import { PostCard, TPost } from '../PostCard'
 
@@ -5,15 +6,40 @@ import styles from './PostListing.module.scss'
 
 interface IPostListingProps {
   posts: TPost[]
+  limit: number
 }
 
-export const PostListing: FC<IPostListingProps> = ({ posts: initialPosts }) => {
-  const [posts] = useState(initialPosts)
+export const PostListing: FC<IPostListingProps> = ({
+  posts: initialPosts,
+  limit
+}) => {
+  const [posts, setPosts] = useState(initialPosts)
+
+  const { loadMore, canLoadMore } = useLoadMore<TPost>(
+    'posts',
+    limit,
+    initialPosts.length
+  )
+
+  const handleLoadMore = async () => {
+    const posts = await loadMore()
+    if (posts) {
+      setPosts(prev => [...prev, ...posts])
+    }
+  }
+
   return (
     <section className={styles.posts}>
       {posts.map(post => (
         <PostCard key={post.id} post={post} />
       ))}
+      {canLoadMore && (
+        <div className={styles.loadMore}>
+          <button className="btn btn-primary" onClick={handleLoadMore}>
+            Carregar Mais
+          </button>
+        </div>
+      )}
     </section>
   )
 }
