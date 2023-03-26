880807d0433235b0a76bdc2b66771bc0f6d6a30e
Pedro-Afonso <pedro.766@hotmail.com>
2023-03-26 10:57:28 -0300

    feat: Add redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { remotePatterns: [{ protocol: 'https', hostname: 'robohash.org' }] }
+  images: { remotePatterns: [{ protocol: 'https', hostname: 'robohash.org' }] },
+  async redirects() {
+    return [{ source: "/", destination: "/posts", permanent: false }];
+  },
 }
 
 module.exports = nextConfig
